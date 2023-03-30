99e0a3ccd15c2966ee7bd50a8645365b510d2fbe
dbarkowsky <dylanbarkowsky@gmail.com>
2023-03-30 11:33:01 -0700

    test3

--- api/controllers/health-api-controller.js
+++ api/controllers/health-api-controller.js
@@ -1,4 +1,5 @@
 // Used to check if API is running. 
 export const healthCheck = async (req, res) => {
+    console.log('test3')
     return res.status(200).send('/health endpoint reached. API running.');
 }
