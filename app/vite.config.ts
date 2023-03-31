3cc0c86381200fbec8006de56f8625b7ece0d5be
dbarkowsky <dylanbarkowsky@gmail.com>
2023-03-31 10:58:07 -0700

    Consolidate gitignore, provide vite port options

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react'; 
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-  },
-})
+ 
+
+export default ({ mode }) => {
+  // In-case the frontend port needs to be changed at some point:
+  // Load app-level env vars to node-level env vars.
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  
+  const port : number = +(process.env.VITE_PORT || 8080);
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      host: true,
+      port: port,
+    },
+  })
+}
