8e4773bb6f28d2df8cf1c5ad1e44fcbb8c96c23c
dbarkowsky <dylanbarkowsky@gmail.com>
2023-04-06 11:42:18 -0700

    Added Cypress to frontend for testing

---
+++ app/cypress.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+    specPattern: "**/component/**/*.cy.tsx",
+    video: false,
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: "**/e2e/**/*.cy.tsx",
+    video: false
+  },
+});
