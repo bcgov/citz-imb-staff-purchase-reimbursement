c579f9d849db39f80097c819958007e49dace279
dbarkowsky <dylanbarkowsky@gmail.com>
2023-04-14 09:15:49 -0700

    Fixed test to accurately insert data.

--- api/tests/integration/api-requests-post.test.js
+++ api/tests/integration/api-requests-post.test.js
@@ -6,27 +6,29 @@ const request = supertest(endpoint);
 describe("Testing POST route for /requests endpoint", () => {
   test("API returns code 201 on successful submission", async () => {
     const response = await request.post("/requests").send({
-      "lateEntry": false,
-      "firstName": "John",
-      "lastName": "Smith",
-      "employeeId": 234567,
-      "itemsPurchased": [
-        "Dogs"
-      ],
-      "totalCost": 45.46,
-      "purchaseDate": "2023-04-05T00:00:00-07:00",
-      "attachReceipts": [
-        {}
-      ],
-      "approvalDate": "2023-04-13T00:00:00-07:00",
-      "attachApproval": [
-        {}
-      ],
-      "supplierName": "Jimmy's Dogs",
-      "supplierPhoneNumber": "(324) 324-2342",
-      "supplierEmail": "jimmys@yahoo.com",
-      "additionalComments": "Great purchase!",
-      "submit": true,
+      data: {
+        lateEntry: false,
+        firstName: "John",
+        lastName: "Smith",
+        employeeId: 234567,
+        itemsPurchased: [
+          "Dogs"
+        ],
+        totalCost: 45.46,
+        purchaseDate: "2023-04-05T00:00:00-07:00",
+        attachReceipts: [
+          {}
+        ],
+        approvalDate: "2023-04-13T00:00:00-07:00",
+        attachApproval: [
+          {}
+        ],
+        supplierName: "Jimmy's Dogs",
+        supplierPhoneNumber: "(324) 324-2342",
+        supplierEmail: "jimmys@yahoo.com",
+        additionalComments: "Great purchase!",
+        submit: true,
+      }
     });
     expect(response.ok).toBe(true);
     expect(response.status).toBe(201);
