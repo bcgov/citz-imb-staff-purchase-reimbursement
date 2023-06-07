9be1b0db187a21ecf4224aff5afa87e527f29953
dbarkowsky <dylanbarkowsky@gmail.com>
2023-06-07 11:52:02 -0700

    Added commenting

--- api/helpers/checkForCompleteRequest.ts
+++ api/helpers/checkForCompleteRequest.ts
@@ -1,12 +1,24 @@
 import { Approval } from "./../interfaces/Approval";
 import { Purchase } from "./../interfaces/Purchase";
 
+/**
+ * @interface
+ * @description The data that must be checked when a request is updated.
+ * @param {number}          employeeId  - The employee ID of the requestor.
+ * @param {Array<Purchase>} purchases   - The purchases in the request.
+ * @param {Array<Approval>} approvals   - The approvals in the request.
+ */
 interface RequestUpdateData {
   employeeId: number,
   purchases: Array<Purchase>,
   approvals: Array<Approval>
 }
 
+/**
+ * @description Checks for possible missing fields or files in the update data of a request.
+ * @param {RequestUpdateData} requestData The update data.
+ * @returns {boolean} True if all data is present. False if anything is missing.
+ */
 export const checkForCompleteRequest = (requestData: RequestUpdateData) => {
   // Are required fields blank?
   if (!requestData.employeeId) return false;
