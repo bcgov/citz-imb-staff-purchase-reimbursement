fa7c98d9098a30d24896286cefef25e208349b74
dbarkowsky <dylanbarkowsky@gmail.com>
2023-06-27 08:27:38 -0700

    Added e2e test for logging in.

--- app/cypress.config.ts
+++ app/cypress.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from 'cypress';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' });
 
 export default defineConfig({
   component: {
@@ -16,5 +19,12 @@ export default defineConfig({
     },
     specPattern: '**/e2e/**/*.cy.tsx',
     video: false,
+    env: {
+      auth_base_url: 'https://dev.loginproxy.gov.bc.ca/auth',
+      auth_logon_url: 'https://logontest7.gov.bc.ca/',
+      auth_client_id: process.env.SSO_CLIENT_ID,
+      test_username: process.env.TEST_USERNAME,
+      test_password: process.env.TEST_PASSWORD,
+    },
   },
 });
--- app/cypress/e2e/LoginPage.cy.tsx
+++ app/cypress/e2e/LoginPage.cy.tsx
@@ -1,9 +1,32 @@
 describe('Login Page', () => {
-  it('Are all elements present on login page?', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:8080');
+  });
+
+  it('Are all elements present on login page?', () => {
     cy.get('h1').contains('Staff Purchase Reimbursement');
     cy.get('button').contains('Log In');
     cy.get('h3').contains('Login Required');
     cy.get('p').contains('To access this application, log in with your IDIR.');
+  });
+
+  it('Log In process gets to Home page.', () => {
+    // Log In Process
+    cy.get('button').click();
+    cy.origin(Cypress.env('auth_base_url'), () => {
+      cy.contains('IDIR').click();
+    });
+    cy.origin(Cypress.env('auth_logon_url'), () => {
+      cy.get('input[name="user"]').click();
+      cy.get('input[name="user"]').type(Cypress.env('test_username'));
+      cy.get('input[name="password"]').click();
+      cy.get('input[name="password"]').type(Cypress.env('test_password'));
+      cy.get('input[name="btnSubmit"').click();
+    });
+    // Should be at home page
+    cy.get('h2').contains('Reimbursement Requests');
+    cy.get('td').contains('No requests available.');
+    // Should not have admin view
+    cy.get('span').contains('Admin view').should('not.exist');
   });
 });
