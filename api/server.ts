b19ba753b1453dbf12952fc72bd1704f6a2a45ae
dbarkowsky <dylanbarkowsky@gmail.com>
2023-06-28 10:43:54 -0700

    Included cron schedule for deletion

--- api/server.ts
+++ api/server.ts
@@ -4,6 +4,7 @@ import db from './db/conn';
 import { Collection } from 'mongodb';
 import { RequestRecord } from './controllers/requests-api-controller';
 import RequestStates from './constants/RequestStates';
+import cron from 'node-cron';
 
 const { API_PORT } = Constants;
 
@@ -14,8 +15,11 @@ app.listen(API_PORT, (err?: Error) => {
   // Deletes files from Mongo if
   // the request has deleted state &&
   // is older than one month
+  /**
+   * @description Deletes files from Mongo if the request has a DELETED state and is older than one month
+   */
   const deleteOldFiles = async () => {
-    console.log('Starting Mongo file cleanup routine.');
+    console.log(`Starting Mongo file cleanup routine at ${new Date().toISOString()}.`);
     const thirtyDaysOfMilliseconds = 1000 * 60 * 60 * 24 * 30;
     const collection: Collection<RequestRecord> = db.collection<RequestRecord>('requests');
     await collection.updateMany(
@@ -30,7 +34,12 @@ app.listen(API_PORT, (err?: Error) => {
         },
       },
     );
-    console.log('Mongo file cleanup complete.');
+    console.log(`Mongo file cleanup complete at ${new Date().toISOString()}.`);
   };
-  deleteOldFiles();
+
+  // minute, hour, day of month, month, day of week
+  const cronSchedule = '* 1 * * 0'; // Runs Sunday at 1am.
+  cron.schedule(cronSchedule, () => {
+    deleteOldFiles();
+  });
 });
