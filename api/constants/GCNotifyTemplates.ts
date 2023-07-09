983b9e0d9645acc8dc2f6e16b3a5b370c193140e
dbarkowsky <dylanbarkowsky@gmail.com>
2023-07-09 12:02:07 -0700

    add more gc notify options

--- api/constants/GCNotifyTemplates.ts
+++ api/constants/GCNotifyTemplates.ts
@@ -3,7 +3,10 @@
  * @constant
  */
 const Templates = {
-  NewRequestNotification: '36c9ab59-c467-4393-bab8-c334f364d0d0',
+  NewRequestNotification: '36c9ab59-c467-4393-bab8-c334f364d0d0', // When a new request is received, notify an admin
+  RemindUserOfIncomplete: '602b7c71-31b1-45ee-935a-bf177bda89bc', // When request has been incomplete for more than a week, notify the requestor
+  NotifyUserOfChange: 'd3ea3626-7182-4577-8f9d-2f277c7fd015', // When an admin changes a record to Incomplete or Complete, notify the requestor
+  // NotifyAdminOfUpdate: '', // When a user updates a record from Incomplete to Submitted, notify an admin
 };
 
 export default Templates;
--- api/helpers/useGCNotify.ts
+++ api/helpers/useGCNotify.ts
@@ -1,21 +1,16 @@
 import axios from 'axios';
 import Templates from '../constants/GCNotifyTemplates';
 
-/**
- * @description Sends an email notifying the recipient of a new request.
- * @param {string}  email The email address for the recipient mailbox.
- * @param {string}  link  The URL needed to access the new request submission.
- */
-export const sendNewRequestNotification = async (email: string, link: string) => {
+const contactGCNotify = async (email: string, url: string, template: string) => {
   const { GC_NOTIFY_API_KEY } = process.env;
   try {
     await axios.post(
       'https://api.notification.canada.ca/v2/notifications/email',
       {
-        template_id: Templates.NewRequestNotification,
+        template_id: template,
         email_address: email,
         personalisation: {
-          url: link,
+          url,
         },
       },
       {
@@ -28,4 +23,21 @@ export const sendNewRequestNotification = async (email: string, link: string) =>
   } catch (e) {
     console.log(e);
   }
+};
+
+/**
+ * @description Sends an email notifying the recipient of a new request.
+ * @param {string}  email The email address for the recipient mailbox.
+ * @param {string}  url  The URL needed to access the new request submission.
+ */
+export const sendNewRequestNotification = (email: string, url: string) => {
+  contactGCNotify(email, url, Templates.NewRequestNotification);
+};
+
+export const sendIncompleteReminder = (email: string, url: string) => {
+  contactGCNotify(email, url, Templates.RemindUserOfIncomplete);
+};
+
+export const sendChangeNotification = (email: string, url: string) => {
+  contactGCNotify(email, url, Templates.NotifyUserOfChange);
 };
