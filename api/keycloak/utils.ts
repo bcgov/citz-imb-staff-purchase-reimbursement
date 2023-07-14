17d7b123512efc017d0e3ba8cc929f07dbdf7896
dbarkowsky <dylanbarkowsky@gmail.com>
2023-07-14 10:48:29 -0700

    Added User interface for keycloak

--- api/keycloak/utils.ts
+++ api/keycloak/utils.ts
@@ -127,3 +127,17 @@ export const getUserInfo = (access_token: string) => {
   if (!data) return null;
   return data.payload;
 };
+
+// Describes the user info contained in the request
+export interface User {
+  idir_user_guid: string;
+  identity_provider: string;
+  idir_username: string;
+  name: string;
+  preferred_username: string;
+  given_name: string;
+  display_name: string;
+  family_name: string;
+  email: string;
+  client_roles?: string[];
+}
